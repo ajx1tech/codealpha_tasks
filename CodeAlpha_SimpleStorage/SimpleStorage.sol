f66123454d864a601c07eedd9e9e0a5d4e32a46a
ajit_sharma <ajx1tech@gmail.com>
2026-05-27 15:01:37 +0530

    Updated code

--- CodeAlpha_SimpleStorage/SimpleStorage.sol
+++ CodeAlpha_SimpleStorage/SimpleStorage.sol
@@ -2,48 +2,18 @@
 pragma solidity ^0.8.0;
 
 /// @title SimpleStorage - CodeAlpha Internship Task 1
-/// @author Ajit Sharma
 /// @notice A simple smart contract to store, increment and decrement an integer value
-/// @dev Demonstrates basic Solidity state variable management and functions
-
 contract SimpleStorage {
 
-    // -------------------------------------------------------
-    // State Variables
-    // -------------------------------------------------------
-
-    /// @notice The stored integer value (publicly readable)
     uint256 public storedValue;
 
-    /// @notice The address of the contract owner
-    address public owner;
-
-    // -------------------------------------------------------
-    // Events
-    // -------------------------------------------------------
-
-    /// @notice Emitted when the value is incremented
     event ValueIncremented(uint256 newValue, address indexed by);
-
-    /// @notice Emitted when the value is decremented
     event ValueDecremented(uint256 newValue, address indexed by);
-
-    /// @notice Emitted when the value is manually set
     event ValueSet(uint256 newValue, address indexed by);
 
-    // -------------------------------------------------------
-    // Constructor
-    // -------------------------------------------------------
-
-    /// @notice Initializes the contract with a starting value of 0
     constructor() {
         storedValue = 0;
-        owner = msg.sender;
     }
-
-    // -------------------------------------------------------
-    // Core Functions
-    // -------------------------------------------------------
 
     /// @notice Increments the stored value by 1
     function increment() external {
@@ -60,14 +30,12 @@ contract SimpleStorage {
     }
 
     /// @notice Sets the stored value to a specific number
-    /// @param _value The new value to store
     function setValue(uint256 _value) external {
         storedValue = _value;
         emit ValueSet(storedValue, msg.sender);
     }
 
     /// @notice Returns the current stored value
-    /// @return The current value of storedValue
     function getValue() external view returns (uint256) {
         return storedValue;
     }
